e72b55297c32b0ea1bc3c77c8d50dd849d4125b4
lizziepaquette <33362730+lizziepaquette@users.noreply.github.com>
2017-11-07 18:47:51 -0500

    Update index.php

--- diary/index.php
+++ diary/index.php
@@ -39,18 +39,9 @@ li a {
 <div style="margin-left:10%;padding: 0px 45px 0px 0px;padding:1px; position:fixed;">
 
 <p>
-1/10/17 2:07am
-Feeling forlorn, a pit of my stomach feeling. The feeling that at any second my heart will start beating much too fast, and I won't be able to keep up. The feeling of being at the top of an emotional roller coaster, anticipating the drop, but facing backwards so I can't see what's coming. 
+At one point I was going to write a diary but then I didn't.
 </p><p>
-I don't know what I'm afraid of. I guess that's been the whole point of this huh. Before therapy, I didn't even know there was something to fear. I thought everyone ran away from the darkness, a life long journey to stay in the limelight. 
-</p><p>
-Now I can tell, (this is starting to sound pretty pretentious but bear with me) there's something there. When I don't get enough done, when I strike out with boys, when I've done something reprehensible. It's a shame, an anxiety, an anticipation of retribution, a fear. I don't want to be alone. But I fear loneliness only because I fear this feeling. Guilt. Unworthiness. When I'm alone I bandaid my feelings with affirmations and promises. I'll do better people like you you're doing fine remember this remember him.. But even though that's self reliant it doesn't deal with the problem. There's still something in me that in trying to avoid. 
-</p><p>
-Even this, which is making me feel better, is only good because I'm producing, and I can show it to you later and you'll approve of me and I'll feel happy. 
-</p><p>
-I don't feel like beating myself up over why I can't feel happy by myself. So that's good. 
-</p><p>
-I'm going to try not making excuses in my head anymore, or even sayin it's okay. I'm just going to feel, truly, deeply, without trepidation. And see what comes of it. Vulnerable not because I was told to be. But because I want to know what's the truth. What's deep down. What's wrong?
+Oops.
 </p>
 <d/div>
 
